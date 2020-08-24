fb093c42fb4ec47605fbbdd450ffb79bd96a16e4
ldgframe <ldgfelipecarrera@gmail.com>
2020-08-24 11:32:38 -0500

    <fix> update claves de nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,14 +60,14 @@ export default {
   firebase: {
     config:{
         production: {
-        apiKey: "",
-        authDomain: "",
-        databaseURL: "",
-        projectId: "",
-        storageBucket: "",
-        messagingSenderId: "1",
-        appId: "",
-        measurementId: ""
+        apiKey: "AIzaSyAFKFFc5NQ8LAiR0KB_FVkUP3s3JEP9jCw",
+        authDomain: "planetapreescolar-39350.firebaseapp.com",
+        databaseURL: "https://planetapreescolar-39350.firebaseio.com",
+        projectId: "planetapreescolar-39350",
+        storageBucket: "planetapreescolar-39350.appspot.com",
+        messagingSenderId: "1085237806661",
+        appId: "1:1085237806661:web:8bf6f3d51794c3a88d452c",
+        measurementId: "G-MTG60YV1RZ"
         },
         development: {
           apiKey: "AIzaSyB8GyvM9PdvjufoAyEZv2uRQLVhVQM-Z-o",
