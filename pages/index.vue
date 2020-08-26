0ad7857243f1bd6515b6a65619c811c0504ca202
ldgfelipe <ldgfelipecarrera@gmail.com>
2020-08-26 12:03:21 -0500

    se eliminan los archivos demo del sistema

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,24 @@
 <template>
- <div>
-  Hola vuejs  {{test}}
-  <Usuarios />
-   </div>
+  <div>
+    Hola vuejs {{ test }}
+    <Usuarios />
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-import Usuarios from '~/components/Usuarios/Usuarios.vue'
-import { mapState } from 'vuex'
-export default {
-  data(){
-    return {
 
-    }
+
+import Usuarios from "~/components/Usuarios/Usuarios.vue";
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {};
   },
-  computed:{
-    ...mapState(['test'])
+  computed: {
+    ...mapState(["test"])
   },
   components: {
-    Logo,
-    VuetifyLogo,
     Usuarios
-  },
-
-
-}
+  }
+};
 </script>
