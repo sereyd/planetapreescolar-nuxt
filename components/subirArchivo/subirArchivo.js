fd5a5ab402ddb640ed44f49ae0952cc2932ca5f2
Ivan Castro <ivanc654gmail.com>
2021-01-21 11:52:58 -0600

    <fix>titulos de secciones, subida de archivos, formularios nuevo recurso y editar recurso, datos de stripe en mi perfil y boton de descarga de los recursos

--- components/subirArchivo/subirArchivo.js
+++ components/subirArchivo/subirArchivo.js
@@ -24,8 +24,8 @@ export default {
       var data = null;
       var metadata = {};
 
-      console.log("SE ESTAN MONTANDO LOS COMPONENTES FILE")
-    console.log(this.urlFileEdit)
+    //   console.log("SE ESTAN MONTANDO LOS COMPONENTES FILE")
+    // console.log(this.urlFileEdit)
 
     if(this.urlFileEdit !== "")
       {
@@ -50,37 +50,37 @@ export default {
       // console.log("CAMBIADO FILE")
       let folder =""
       
-      console.log("this.ubi")
-      console.log(this.ubi)
-      console.log("this.foldercode")
-      console.log(this.foldercode)
-      console.log("this.file")
-      console.log(this.file)
+      // console.log("this.ubi")
+      // console.log(this.ubi)
+      // console.log("this.foldercode")
+      // console.log(this.foldercode)
+      // console.log("this.file")
+      // console.log(this.file)
 
-      console.log("this.nick")
-      console.log(this.nickFile)
+      // console.log("this.nick")
+      // console.log(this.nickFile)
 
-      console.log("this.urlFileEdit")
-      console.log(this.urlFileEdit)
+      // console.log("this.urlFileEdit")
+      // console.log(this.urlFileEdit)
       //SI SE CREARA UN NUEVO RECURSO
       if(!this.file && this.foldercode === "")
       {
         folder = this.$codegenerate()
         this.$emit("updateFoldercode",folder);
-        console.log("folder era vacio")
-        console.log(folder)
+        // console.log("folder era vacio")
+        // console.log(folder)
 
         this.ubi = `${this.subtipo}/${this.datosUsuario.id}/${folder}/`;
-        console.log("ubicacion final")
-        console.log(this.ubi)
+        // console.log("ubicacion final")
+        // console.log(this.ubi)
 
       }
       //SI SE CAMBIA EL ARCHIVO Y EL RECURSO AUN NO SE CREA O CUANDO SE DESEA EDITAR UN RECURSO
       else if((this.file || !this.file) && this.foldercode !== "")
       {
         this.ubi = `${this.subtipo}/${this.datosUsuario.id}/${this.foldercode}/`;
-        console.log("ubicacion final")
-        console.log(this.ubi)
+        // console.log("ubicacion final")
+        // console.log(this.ubi)
         folder  = this.foldercode
       }
       //SI SE CIERRA EL DIALOG CUANDO ESTAS CREANDO UN NUEVO RECURSOS
@@ -89,63 +89,10 @@ export default {
         folder = arrayUbi[2];
 
       }
-     
-
-
-
-      // if(this.ubi === "")
-      // {
-      //   // this.foldercode = this.$codegenerate();
-        
-      //   console.log("this.foldercode")
-      //   console.log(this.foldercode)
-      //   if(this.foldercode === "")
-      //   {
-      //     folder = this.$codegenerate()
-      //     this.$emit("updateFoldercode",folder);
-      //     console.log("folder era vacio")
-      //     console.log(folder)
-      //   }
-      //   else{
-      //     folder = this.foldercode;
-      //     console.log("folder no vacio")
-      //     console.log(folder)
-      //   }
-
-      //   this.ubi = `${this.subtipo}/${this.datosUsuario.id}/${folder}/`;
-      //   console.log("ubicacion final")
-      //   console.log(this.ubi)
-
-      // }
-      // else{
-      //   let arrayUbi =  this.ubi.split("/");
-      //   folder = arrayUbi[2];
-      // }
-
-      // console.log("UBICACION")
-      // console.log(this.ubi)
-      // alert("STOPPP");
 
       this.completado = false;
       this.cargando = true;
       this.porcentaje = 0;
-
-     
-
-      //SE OBTIENE EL TIPO DE ARCHIVO
-      // let res = this.file.type.split("/");
-      
-      // if(res[0] === "application")
-      // {
-      //   console.log("appl")
-      //   res = this.file.name.split(".");
-      //   console.log(res)
-      //   res[0] = res[1];
-      // }
-      // this.$emit("updateTipoFile", res[0]);
-
-      console.log("this.fileInterno")
-      console.log(this.fileInterno)
 
       if(typeof this.fileInterno !== 'undefined')
       {
@@ -164,8 +111,8 @@ export default {
             //SE AGREGA LA FOTO AL STORAGE DE FIREBASE
   
             let storageRef = this.$fireStorage.ref(this.ubi);
-            console.log("nombre del archivo")
-            console.log(this.nickFile+"_"+folder);
+            // console.log("nombre del archivo")
+            // console.log(this.nickFile+"_"+folder);
             let uploadTask = storageRef.child(this.nickFile+"_"+folder).put(file, metadata);
   
             await uploadTask.on('state_changed', // or 'state_changed'
@@ -179,8 +126,8 @@ export default {
                 uploadTask.snapshot.ref.getDownloadURL()
                 .then( async(downloadURL) => {
                     this.urlFile = downloadURL;
-                    console.log('File available at', downloadURL);
-                    console.log('URLFILE', this.urlFile);
+                    // console.log('File available at', downloadURL);
+                    // console.log('URLFILE', this.urlFile);
                     this.$emit("updateUrlFile", this.urlFile);
   
                     
@@ -196,8 +143,6 @@ export default {
 
         this.cargando = false;
       }
-
-      // this.porcentaje = 100;
 
     },
 
